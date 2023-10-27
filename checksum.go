f123999dbabc6e0719e89a6cb1940d79708a7cbc
Lorenzo Milicia <services@lorenzomilicia.dev>
2023-10-27 08:36:17 +0200

    Change API to include error and validate if hash.Hash is linked to the binary.

--- checksum.go
+++ checksum.go
@@ -10,16 +10,21 @@ import (
 	"io"
 )
 
-func Checksum(r io.Reader, checksum string, algorithm crypto.Hash) bool {
-	hash := algorithm.New()
-	if _, err := io.Copy(hash, r); err != nil {
-		return false
+func Validate(r io.Reader, checksum string, hashFunction crypto.Hash) (bool, error) {
+	if !hashFunction.Available() {
+		return false, errors.New(fmt.Sprintf("Hash function %v not implemented", hashFunction.String()))
 	}
+
+	hash := hashFunction.New()
+	if _, err := io.Copy(hash, r); err != nil {
+		return false, err
+	}
+	
 	stringHash := hex.EncodeToString(hash.Sum(nil))
 
 	if stringHash == checksum {
-		return true
+		return true, nil
 	}
 
-	return false
+	return false, nil
 }
