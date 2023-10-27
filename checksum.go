6ed3693e7b1b03bb6a2cefa94733f7f506a0ffdf
Lorenzo Milicia <info@lorenzomilicia.dev>
2023-10-27 14:30:41 +0200

    Handle errors for non available hash functions + write tests (#2)
    * Change API to include error and validate if hash.Hash is linked to the binary.
    
    * Add missing imports
    
    * Add tests

--- checksum.go
+++ checksum.go
@@ -7,19 +7,26 @@ import (
 	_ "crypto/sha256"
 	_ "crypto/sha512"
 	"encoding/hex"
+	"errors"
+	"fmt"
 	"io"
 )
 
-func Checksum(r io.Reader, checksum string, algorithm crypto.Hash) bool {
-	hash := algorithm.New()
-	if _, err := io.Copy(hash, r); err != nil {
-		return false
+func Validate(r io.Reader, checksum string, hashFunction crypto.Hash) (bool, error) {
+	if !hashFunction.Available() {
+		return false, errors.New(fmt.Sprintf("Hash function %v not implemented", hashFunction.String()))
 	}
+
+	hash := hashFunction.New()
+	if _, err := io.Copy(hash, r); err != nil {
+		return false, err
+	}
+
 	stringHash := hex.EncodeToString(hash.Sum(nil))
 
 	if stringHash == checksum {
-		return true
+		return true, nil
 	}
 
-	return false
+	return false, nil
 }
