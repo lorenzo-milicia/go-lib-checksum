176ffc0eba5829434d2fc74586fc0c88fb081c38
Lorenzo Milicia <info@lorenzomilicia.dev>
2023-11-05 09:04:39 +0100

    Feature: Expose new "Hash" function to compute hashes of a file (#3)
    * Define function "Hash"
    
    * Write tests
    
    * Restrict build workflow only on main pushes

--- checksum.go
+++ checksum.go
@@ -13,20 +13,31 @@ import (
 )
 
 func Validate(r io.Reader, checksum string, hashFunction crypto.Hash) (bool, error) {
-	if !hashFunction.Available() {
-		return false, errors.New(fmt.Sprintf("Hash function %v not implemented", hashFunction.String()))
-	}
-
-	hash := hashFunction.New()
-	if _, err := io.Copy(hash, r); err != nil {
+	stringHash, err := computeHash(hashFunction, r)
+	if err != nil {
 		return false, err
 	}
-
-	stringHash := hex.EncodeToString(hash.Sum(nil))
 
 	if stringHash == checksum {
 		return true, nil
 	}
 
 	return false, nil
+}
+
+func Hash(r io.Reader, hashFunction crypto.Hash) (string, error) {
+	return computeHash(hashFunction, r)
+}
+
+func computeHash(hashFunction crypto.Hash, r io.Reader) (string, error) {
+	if !hashFunction.Available() {
+		return "", errors.New(fmt.Sprintf("Hash function %v not implemented", hashFunction.String()))
+	}
+	hash := hashFunction.New()
+	if _, err := io.Copy(hash, r); err != nil {
+		return "", err
+	}
+
+	stringHash := hex.EncodeToString(hash.Sum(nil))
+	return stringHash, nil
 }
